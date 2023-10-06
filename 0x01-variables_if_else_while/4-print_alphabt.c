80cc86a1b1c6f6feb56fce26b23fd9dbb922c2a1
elkallali <r.elkallali@gmail.com>
2023-10-06 15:09:42 -0700

    task completed

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int character;
+
+	for (character = 'a'; character < 'z'; character++)
+	{
+		if (character == 'e' || character == 'q')
+			continue;
+		putchar(character);
+	}
+	putchar('\n');
+	return (0);
+}
