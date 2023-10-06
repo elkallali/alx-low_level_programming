c91283b8513c9f593cc5f621afdd3723d5a0ebcb
elkallali <r.elkallali@gmail.com>
2023-10-06 15:09:24 -0700

    task completed

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int character;
+
+	for (character = 97; character < 123; character++)
+		putchar(character);
+	for (character = 65; character < 91; character++)
+		putchar(character);
+	putchar('\n');
+	return (0);
+}
