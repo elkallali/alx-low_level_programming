ad4f4a18cb0f13b969bb9104062ea0f1af3bb9b4
elkallali <r.elkallali@gmail.com>
2023-10-10 15:46:05 -0700

    task completed

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * main - Entry Point
+ *
+ * Return: 0 value
+ *
+ */
+
+int main(void)
+{
+
+	putchar('_');
+	putchar('p');
+	putchar('u');
+	putchar('t');
+	putchar('c');
+	putchar('h');
+	putchar('a');
+	putchar('r');
+	putchar('\n');
+	return (0);
+}
