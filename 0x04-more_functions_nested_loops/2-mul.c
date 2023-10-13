53f9497f730de3ba491d996aa69a4c28748a7990
elkallali <r.elkallali@gmail.com>
2023-10-12 17:34:04 -0700

    task completed

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * mul - function that checks for a digit (0 through 9).
+ *
+ * @a: first number
+ * @b: second number
+ *
+ * Return: multiplie of @a * @b.
+ */
+int mul(int a, int b)
+{
+		return (a * b);
+}
